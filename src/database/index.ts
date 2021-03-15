0f82ae5892ca86ae019fe5aebbf9ff5151908e05
Raufo Jr <raufo.rastrero@hotmail.com>
2021-03-15 14:47:33 -0300

    first commitn

---
+++ src/database/index.ts
@@ -0,0 +1,3 @@
+import { createConnection } from 'typeorm';
+
+createConnection();  //-> busca as informações do arquico ormconfig.json pelo typeorm
---
+++ src/server.ts
@@ -0,0 +1,55 @@
+
+/*
+Comandos
+yarn init -y             no terminal para iniciar o pacote json no projeto
+code .
+
+Terminal no vs code   ' Sempre dentro do projeto'
+yarn add express
+yarn add typescript -D
+yarn tsc --init
+
+descomentar as linhas no arquivo tsconfig.json
+"outDir": "./dist",
+"rootDir": "./src",
+
+Terminal no vs code
+yarn tsc
+yarn add @types/express -D     -> instala declaração de tipos para o express dentro do typescript
+
+*/
+
+
+/*
+//yarn tsc
+//node dist/server.js
+
+// criar no packge.json
+
+"scripts": {
+    "build": "tsc",
+    "dev:server": "ts-node-dev --ignore-watch node_modules  src/server.ts"
+  },
+//yarn build
+
+apagar a pasta dist
+
+yarn add ts-node-dev -D
+
+yarn dev:server  -> faz o papel so comando //yarn tsc e /node dist/server.js
+*/
+import 'reflect-metadata'; //yarn add reflect-metadata
+import express from 'express';
+
+import './database';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+     console.log('🚀 Server started on port 3333');
+});
